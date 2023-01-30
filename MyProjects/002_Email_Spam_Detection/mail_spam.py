39111d9db4309b808a0e8613d751eaa9ac90cfdf
Vivekanand <itsvivekanand@yahoo.com>
2023-01-30 22:38:24 +0530

    Update mail_spam.py

--- MyProjects/002_Email_Spam_Detection/mail_spam.py
+++ MyProjects/002_Email_Spam_Detection/mail_spam.py
@@ -3,9 +3,23 @@ import numpy as np
 import streamlit as st
 import sklearn
 import joblib
+from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 cv=CountVectorizer()
-model=joblib.load('mymodel.pkl')
+from sklearn.naive_bayes import MultinomialNB
+data = pd.read_csv("spam.csv", encoding= 'latin-1')
+df=data.copy()
+df=df[['v1','v2']]
+df.columns=['target','emails']
+X=np.array(df['emails'])
+y=np.array(df['target'])
+X=cv.fit_transform(X)
+# train test split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=0)
+model = MultinomialNB()
+model.fit(X_train,y_train)
+
+# model=joblib.load('mymodel.pkl')
 
 # model saving
 st.title('Email Spam Detection')
